870db87949d3ce44576b3f578eb78d92c05cc545
nafees nazik <nafeesnazik21@gmail.com>
2026-02-20 07:58:10 +0530

    fix: update import path for utility functions in Switch component

--- src/mainview/components/ui/switch.tsx
+++ src/mainview/components/ui/switch.tsx
@@ -1,6 +1,6 @@
 import { Switch as SwitchPrimitive } from "@base-ui/react/switch";
 
-import { cn } from "~/mainview/lib/utils";
+import { cn } from "~/lib/utils";
 
 function Switch({ className, ...props }: SwitchPrimitive.Root.Props) {
   return (
